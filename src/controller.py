e775f9d12c6e29c9efd6c65078a1095ffffcbdd1
David Song <davsong@cs.washington.edu>
2024-11-18 11:33:17 -0800

    VOLTA-9 initial commit for controller-worker architecture

---
+++ src/controller.py
@@ -0,0 +1,105 @@
+import asyncio
+import grpc
+import anycast_pb2
+import anycast_pb2_grpc
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    stream=sys.stdout
+)
+logger = logging.getLogger(__name__)
+
+
+class Controller:
+    """Abstract class for the controller."""
+
+    def __init__(self, server_addresses):
+        """Store internal state for the controller.
+
+        Args:
+            server_addresses (list[str]): Worker IP + port to connect to.
+
+        Fields:
+            server_addresses (list[str]): Workers - we use the IP + port as a
+            uuid for each worker.
+            stubs (dict): worker -> gRPC stubs.
+            background_tasks (dict): worker -> coroutine task.
+        """
+        self.server_addresses = server_addresses
+        self.stubs = {address: anycast_pb2_grpc.AnycastServiceStub(grpc.aio.insecure_channel(address)) for address in self.server_addresses}
+        self.background_tasks = {address: None for address in self.server_addresses}
+
+    def handle_reply(self, response):
+        """Callback function to handle replies from the workers.
+
+        Args:
+            response (ForwardPacket): gRPC response from the worker.
+
+        Raises:
+            NotImplementedError: Each implementation of the controller
+            must implement this method.
+        """
+        raise NotImplementedError("Implement this method")
+
+    def run_receiver_agents(self, /, hmac, filter):
+        """Initiates each worker receiver agent.
+
+        Creates a coroutine task for each worker to handle streaming
+        responses. Whenever a response is received, it invokes the
+        handle_reply callback function to process the response.
+
+        All tasks are stored in the controller's state background_tasks.
+
+        Args:
+            hmac (str): HMAC to initialize each worker with.
+            filter (str): The filter criteria to initialize each worker with.
+        """
+        async def handle_stream(address):
+            try:
+                logger.info(f"Connecting to {address} for ReceiverAgent")
+                async for response in self.stubs[address].ReceiverAgent(anycast_pb2.InitRequest(hmac=hmac, filter=filter, host=address)):
+                    self.handle_reply(response)
+            except grpc.aio.AioRpcError as e:
+                logger.error(f"Error from {address}: {e}")
+
+        for address in self.server_addresses:
+            task = asyncio.create_task(handle_stream(address))
+            self.background_tasks[address] = task
+
+    def stop_receiver_agents(self):
+        """Stop all the receiver agents.
+
+        Cancel the tasks. This closes the gRPC stream and causes
+        the receiver agents to exit.
+        """
+        for task in self.background_tasks.values():
+            task.cancel()
+        asyncio.run(asyncio.gather(*self.background_tasks, return_exceptions=True))
+
+    async def send_packet(self, address, raw_request):
+        """
+        Sends a packet to the specified address.
+
+        Args:
+            address (str): The worker to forward the packet from.
+            raw_request (bytes): The L3 raw packet to be sent.
+
+        Returns:
+            None: if there is an error.
+            CommandResponse: gRPC response object.
+            - Status code indicating if the packet was sent
+            - Timestamp (ISO format) of when the packet was sent
+              from the worker.
+        """
+        if address not in self.stubs:
+            logger.error(f"Address {address} is not in the list of servers.")
+            return
+
+        try:
+            response = await self.stubs[address].SendPacket(anycast_pb2.SendRequest(raw_request=raw_request))
+            return response
+        except grpc.aio.AioRpcError as e:
+            logger.error(f"Error sending packet to {address}: {e}")
--- src/server.py
+++ src/server.py
@@ -1,29 +1,109 @@
 import grpc
+from queue import Queue, Empty
+from threading import Thread
 from concurrent import futures
-import subprocess
-import command_service_pb2
-import command_service_pb2_grpc
+import anycast_pb2
+import anycast_pb2_grpc
+from scapy.all import sendp, sniff, Ether
+import logging
+import sys
+from datetime import datetime
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    stream=sys.stdout
+)
+logger = logging.getLogger(__name__)
 
 
-class CommandServiceServicer(command_service_pb2_grpc.CommandServiceServicer):
-    def RunCommand(self, request, context):
+class AnycastServiceServicer(anycast_pb2_grpc.AnycastServiceServicer):
+    def __init__(self):
+        """Store internal state for the worker agent.
+
+        Fields:
+            hmac (bytes): authenticate incoming packets to prevent abuse.
+            host (int): ID of the worker agent assigned by the controller.
+            queue (Queue): stores packets captured by the sniffer.
+
+        In the future, we aim to replace the HMAC and filter option with an
+        kernel eBFP filter for better performance.
+        """
+        self.hmac = None
+        self.host = None
+        self.queue = Queue()
+
+    def sniffer(self, filter):
+        """Capture packets and put them in a queue.
+
+        Because the sniff function is synchronous, it is run in a separate
+        thread to avoid blocking the gRPC server.
+
+        Args:
+            filter (string): Set a filter on sniff to only capture desired
+            packets.
+        """
+        sniff(prn=lambda packet: self.queue.put((packet, datetime.now())), filter=filter, store=False)
+
+    def ReceiverAgent(self, request, context):
+        """Initialize worker agent to forward packets.
+
+        Args:
+            request (InitRequest): gRPC request object. It sets state at
+            the worker agent and starts a sniffer thread to capture packets.
+            - HMAC
+            - Host ID
+            - Filter
+
+        Yields:
+            ForwardPacket: gRPC response stream object.
+            - Raw packet
+            - Host ID
+            - Receive timestamp (ISO format)
+        """
+        self.hmac = request.hmac
+        self.host = request.host
+        sniffer_thread = Thread(target=self.sniffer, args=(request.filter,))
+        sniffer_thread.start()
+        while context.is_active():
+            try:
+                packet, timestamp = self.queue.get(timeout=1)
+                yield anycast_pb2.ForwardPacket(raw_reply=packet, host=self.host, receive_time=timestamp.isoformat())
+            except Empty:
+                logger.info("No packets received")
+                continue
+        sniffer_thread.join()
+        # TODO: anything else to clean up + safely exit.
+
+    def SendPacket(self, request, context):
+        """Send a raw packet from the worker agent.
+
+        Args:
+            request (SendRequest): gRPC request object.
+            - Raw packet
+
+        Returns:
+            CommandResponse: gRPC response object.
+            - Status code indicating if the packet was sent
+            - Sent timestamp (ISO format)
+        """
         try:
-            result = subprocess.run(request.command, shell=True, capture_output=True, text=True)
-            output = result.stdout
-            error = result.stderr
-        except Exception as e:
-            output = ""
-            error = str(e)
-        return command_service_pb2.CommandResponse(output=output, error=error)
+            logger.info("Sending packet")
+            sent_time = datetime.now().isoformat()
+            sendp(Ether(request.raw_request), iface="ens3")
+            return anycast_pb2.CommandResponse(code=200, sent_time=sent_time)
+        except Exception:
+            return anycast_pb2.CommandResponse(code=500)
 
 
-def serve():
+def serve(port):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    command_service_pb2_grpc.add_CommandServiceServicer_to_server(CommandServiceServicer(), server)
-    server.add_insecure_port('0.0.0.0:50051')
+    anycast_pb2_grpc.add_AnycastServiceServicer_to_server(AnycastServiceServicer(), server)
+    server.add_insecure_port(f'0.0.0.0:{port}')
     server.start()
+    logger.info(f"Server started on port {port}")
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    serve()
+    serve(port=50051)
